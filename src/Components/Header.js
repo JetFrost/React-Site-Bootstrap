20203af7672f88c38634471743ad640ffc840c92
JetFrost <bugaj.anton01@gmail.com>
2021-01-15 12:43:25 +0200

    finalization

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav">
-                            <Nav className="mr-auto">
+                            <Nav className="ml-auto mr-4">
                                 <Nav.Link href="/">Home</Nav.Link>
                                 <Nav.Link href="/about">About us</Nav.Link>
                                 <Nav.Link href="/contacts">Contacts</Nav.Link>
